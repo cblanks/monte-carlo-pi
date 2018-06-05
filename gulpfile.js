edeecd4fd5dbadcd3c5e169d869ab39483106c48
Chris Blanks <chris.blanks@gmail.com>
2018-06-05 23:06:53 +0100

    building page with scss and gulp.  serving with browsersync and gulp for dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,50 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var htmlreplace = require('gulp-html-replace');
- 
+var bs = require('browser-sync').create();
+
 gulp.task('default', function() {
-  gulp.src('index.html')
-    .pipe( htmlreplace({
-      'js': 'https://d3js.org/d3.v4.min.js'
-    }) )
-    .pipe(gulp.dest('build/'));
+  gulp
+    .src('index.html')
+    .pipe(
+      htmlreplace({
+        css: {
+          src: gulp.src('style.scss').pipe(sass()),
+          tpl: '<style>%s</style>'
+        }
+      })
+    )
+    .pipe(
+      htmlreplace({
+        'js': {
+          src: gulp.src('node_modules/d3/build/d3.min.js'),
+          tpl: '<script>%s</script>'
+        }
+      })
+    )
+    .pipe(
+      gulp.dest('build/')
+    )
+  }
+);
+
+gulp.task('browser-sync', function() {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
+
+gulp.task('sass', function () {
+  return gulp.src('style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+    .pipe(bs.reload({ stream: true }));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('style.scss', ['sass']);
+});
+
+gulp.task('watch', ['browser-sync', 'sass:watch']);
